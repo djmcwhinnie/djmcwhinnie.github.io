c6b9b19f7e947370ee6fce4ccfee51556d6db39b
Douglas McWhinnie <hello@dmcwhinnie.co.uk>
2020-08-27 00:30:19 +0100

    Full Update
    Complete update, removing use of bootstrap and just using modern CSS
    and semantic markup.
    
    Updating all content, images, icons and copy as well as updating brand
    logo.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,15 +1,30 @@
-// Remove Phantom Text Nodes
-  function clean(node)
+// Remove Phantom Text Nodes - Firefox Only?
+function clean(node)
+{
+  for(var n = 0; n < node.childNodes.length; n ++)
   {
-    for(var n = 0; n < node.childNodes.length; n ++)
-    {
-      var child = node.childNodes[n];
-      if( child.nodeType === 8 || (child.nodeType === 3 && !/\S/.test(child.nodeValue)) ) {
-        node.removeChild(child);
-        n --;
-      } else if(child.nodeType === 1) {
-        clean(child);
-      }
+    var child = node.childNodes[n];
+    if( child.nodeType === 8 || (child.nodeType === 3 && !/\S/.test(child.nodeValue)) ) {
+      node.removeChild(child);
+      n --;
+    } else if(child.nodeType === 1) {
+      clean(child);
     }
   }
-  clean(document);
+}
+clean(document);
+
+// Flickety Carousel
+var flkty = new Flickity( '.design__carousel', {
+  cellAlign: 'left',
+  contain: true
+});
+
+var flkty = new Flickity( '.hobbies__carousel', {
+  cellAlign: 'left',
+  contain: true
+});
+
+// Footer Date
+var footerDate = new Date();
+document.getElementById("footer-date").innerHTML = footerDate.getFullYear();
